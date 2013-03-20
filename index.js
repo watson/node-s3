7f32ccedb7955d01aad18545158aa6190cd5dbb1
Thomas Watson Steen <w@tson.dk>
2013-03-20 22:03:42 +0100

    Upgrade to curly 0.6.x

--- index.js
+++ index.js
@@ -1,72 +1,77 @@
-var curly = require('curly').pool(false);
-var common = require('common');
+"use strict";
+
+var curly = require('curly');
 var crypto = require('crypto');
 var parseURL = require('url').parse;
 
-var signer = function(publicKey, secretKey) {
-  var stringToSign = function(method, resource, headers) {
+var signer = function (publicKey, secretKey) {
+  var stringToSign = function (verb, resource, headers) {
     var res = [
-      method,
+      verb,
       headers['content-md5'] || '',
       headers['content-type'] || '',
       ''
     ];
 
-    if (headers['x-amz-acl']) {
-      res.push('x-amz-acl:'+headers['x-amz-acl']);
-    }
-    if (headers['x-amz-copy-source']) {
-      res.push('x-amz-copy-source:'+headers['x-amz-copy-source']);
-    }
-    if (headers['x-amz-date']) {
-      res.push('x-amz-date:'+headers['x-amz-date']);
-    }
-    if (headers['x-amz-storage-class']) {
-      res.push('x-amz-storage-class:'+headers['x-amz-storage-class']);
-    }
+    if (headers['x-amz-acl'])
+      res.push('x-amz-acl:' + headers['x-amz-acl']);
+    if (headers['x-amz-copy-source'])
+      res.push('x-amz-copy-source:' + headers['x-amz-copy-source']);
+    if (headers['x-amz-date'])
+      res.push('x-amz-date:' + headers['x-amz-date']);
+    if (headers['x-amz-storage-class'])
+      res.push('x-amz-storage-class:' + headers['x-amz-storage-class']);
 
     res.push(resource);
 
     return res.join('\n');
-  }
+  };
 
-  return function(method, resource, headers) {
+  return function (verb, resource, headers) {
     headers = headers || {};
     headers.date = new Date().toUTCString();
     headers['x-amz-date'] = headers.date;
-    headers.authorization = 'AWS '+
-      publicKey+':'+crypto.createHmac('sha1',
-          secretKey).update(stringToSign(method, resource,
-              headers)).digest('base64');
+
+    var hash = crypto.createHmac('sha1', secretKey).update(stringToSign(verb, resource, headers)).digest('base64');
+
+    headers.authorization = 'AWS ' + publicKey + ':' + hash;
 
     return headers;
   };
 };
 
-exports.create = function(options) {
+exports.create = function (options) {
+  var that = {},
+      auth, publicKey, secretKey, sign, prefix;
+
   options = typeof options === 'string' ? parseURL(options) : options;
 
-  var that = {};
-
-  that.pathname = (options.pathname || '');
+  that.pathname = options.pathname || '';
   that.bucket = options.hostname.split('.')[0];
 
-  var publicKey = options.auth.split(':')[0];
-  var secretKey = options.auth.split(':')[1];
+  auth = options.auth.split(':');
+  publicKey = auth[0];
+  secretKey = auth[1];
 
-  var sign = signer(publicKey, secretKey);
-  var prefix = that.bucket+'.s3.amazonaws.com'+that.pathname;
+  sign = signer(publicKey, secretKey);
+  prefix = that.bucket + '.s3.amazonaws.com' + that.pathname;
 
-  ['put', 'post', 'get', 'del', 'head'].forEach(function(method) {
-    var httpMethod = method.replace('del', 'delete').toUpperCase();
+  ['put', 'post', 'get', 'del', 'head'].forEach(function (method) {
+    var verb = method.replace('del', 'delete').toUpperCase();
 
-    that[method] = function(pathname, headers, callback) {
-      if (typeof headers === 'function') {
-        callback = headers;
-        headers = {};
+    that[method] = function (pathname, options, callback) {
+      var signing = '/' + that.bucket + that.pathname + pathname;
+
+      if (typeof options === 'function') {
+        callback = options;
+        options = {};
       }
-      var signing = '/'+that.bucket+that.pathname+pathname;
-      return curly[method](prefix+pathname).headers(sign(httpMethod, signing, headers), callback);
+
+      options = options || {};
+      options.pool = false;
+      options.headers = sign(verb, signing, options.headers);
+
+      return curly[method](prefix + pathname, options, callback);
     };
   });
 
